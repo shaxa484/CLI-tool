6281a385e61a03d8b3c20dbef3cda248fd837600
Do'ngboyev Shaxriyor <dongboyevsh@gmail.com>
2026-07-05 18:11:17 +0500

    commands

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,2 +1,22 @@
 #!/usr/bin/env node
-console.log('Hello terminal!');
+try{
+    const arg=require('arg');
+
+    const args=arg({
+        '--start':Boolean,
+        '--build':Boolean,
+    });
+
+    if(args['--start']){
+        console.log('Starting the application...');
+
+}} catch (error) {
+    console.log('An error occurred:', error.message);
+    console.log();
+    usage();
+}
+ function usage() {
+    console.log('tool [CMD]');
+    console.log('  --start   Start the application');
+    console.log('  --build   Build the application');
+}
