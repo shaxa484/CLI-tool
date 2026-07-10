d179f10a6f3f97c445d4939f9cc416513ece0bd7
Do'ngboyev Shaxriyor <dongboyevsh@gmail.com>
2026-07-11 01:57:36 +0500

    a bit og coloring

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,22 +1,43 @@
 #!/usr/bin/env node
-try{
-    const arg=require('arg');
+import arg from 'arg';
+import chalk from 'chalk';
+import path from 'path';
 
+try{
+    
     const args=arg({
         '--start':Boolean,
         '--build':Boolean,
+        '--help':Boolean,
     });
 
     if(args['--start']){
-        console.log('Starting the application...');
+        const pkgPath = pkgUp.sync({cwd: process.cwd()});
+        const pkg = require(pkgPath);
+        if (pkg.tool) {
+      console.log('Found configuration', pkg.tool);
+      // TODO: do something with configuration
+    } else {
+      console.log(chalk.yellow('Could not find configuration, using default'));
+      // TODO: get default configuration
+    }
+        console.log(chalk.green('Starting the application...'));
+    }
+    if(args['--build']){
+        console.log(chalk.blue('Building the application...'));
+    }
+    if(args['--help']){
+        usage();
+    }
 
-}} catch (error) {
-    console.log('An error occurred:', error.message);
+} catch (error) {
+    console.log(chalk.yellow(('An error occurred:'), error.message));
     console.log();
     usage();
 }
  function usage() {
-    console.log('tool [CMD]');
-    console.log('  --start   Start the application');
-    console.log('  --build   Build the application');
+    console.log(`${chalk.whiteBright('tool [CMD]')} 
+    ${chalk.greenBright('--start  Start the application')}
+    ${chalk.greenBright('--build  Build the application ')} 
+    ${chalk.greenBright('--help   Show this help message\n')}`);
 }
